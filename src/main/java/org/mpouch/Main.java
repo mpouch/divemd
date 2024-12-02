e9a965e03ace429cefb7925ab0acaa393f43ccd8
mpouch <177807460+mpouch@users.noreply.github.com>
2024-12-01 23:28:18 -0300

    Create main frame

--- src/main/java/org/mpouch/Main.java
+++ src/main/java/org/mpouch/Main.java
@@ -1,7 +1,19 @@
 package org.mpouch;
 
+import org.mpouch.ui.frames.MainFrame;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame frame = new MainFrame();
+                frame.setVisible(true);
+            }
+        });
+
     }
 }
---
+++ src/main/java/org/mpouch/ui/frames/MainFrame.java
@@ -0,0 +1,16 @@
+package org.mpouch.ui.frames;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+
+        // Config
+        setTitle("divemd");
+        setSize(800, 600);
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
