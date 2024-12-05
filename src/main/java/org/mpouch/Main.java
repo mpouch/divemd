bb86a78e55936417ed81c6c0a4903a04e031dfee
mpouch <177807460+mpouch@users.noreply.github.com>
2024-12-05 00:34:58 -0300

    Set UI config

--- src/main/java/org/mpouch/Main.java
+++ src/main/java/org/mpouch/Main.java
@@ -2,6 +2,7 @@ package org.mpouch;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import org.mpouch.ui.MainFrame;
+import org.mpouch.ui.config.UIManagerConfig;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,6 +13,7 @@ public class Main {
         try {
             UIManager.put("defaultFont", new Font("Dialog", Font.PLAIN, 13));
             UIManager.setLookAndFeel(new FlatLightLaf());
+            UIManagerConfig.configureUI();
         } catch (UnsupportedLookAndFeelException e) {
             throw new RuntimeException(e);
         }
---
+++ src/main/java/org/mpouch/ui/config/UIManagerConfig.java
@@ -0,0 +1,25 @@
+package org.mpouch.ui.config;
+
+import javax.swing.*;
+import javax.swing.plaf.IconUIResource;
+
+public class UIManagerConfig {
+
+    public static void configureUI() {
+        UIManager.put("Tree.paintLines", true);
+        UIManager.put("Tree.showDefaultIcons", true);
+
+        // Icons
+        ImageIcon collapsedIcon = new ImageIcon(UIManagerConfig.class.getResource("/icons/bullet_toggle_plus.png"));
+        ImageIcon expandedIcon = new ImageIcon(UIManagerConfig.class.getResource("/icons/bullet_toggle_minus.png"));
+        ImageIcon openIcon = new ImageIcon(UIManagerConfig.class.getResource("/icons/folder.png"));
+        ImageIcon closedIcon = new ImageIcon(UIManagerConfig.class.getResource("/icons/folder.png"));
+        ImageIcon leafIcon = new ImageIcon(UIManagerConfig.class.getResource("/icons/page_white_text.png"));
+
+        UIManager.put("Tree.collapsedIcon", new IconUIResource(collapsedIcon));
+        UIManager.put("Tree.expandedIcon", new IconUIResource(expandedIcon));
+        UIManager.put("Tree.openIcon", new IconUIResource(openIcon));
+        UIManager.put("Tree.closedIcon", new IconUIResource(closedIcon));
+        UIManager.put("Tree.leafIcon", new IconUIResource(leafIcon));
+    }
+}
