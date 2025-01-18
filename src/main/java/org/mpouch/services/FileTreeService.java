0deaed266413c46f6a3afa6b22e503c561a6ec7b
mpouch <177807460+mpouch@users.noreply.github.com>
2025-01-18 01:28:30 -0300

    FileTree: sort tree nodes by file type and then by file name

--- src/main/java/org/mpouch/services/FileTreeService.java
+++ src/main/java/org/mpouch/services/FileTreeService.java
@@ -2,6 +2,8 @@ package org.mpouch.services;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class FileTreeService {
 
@@ -19,10 +21,25 @@ public class FileTreeService {
         DefaultMutableTreeNode node = new DefaultMutableTreeNode(file);
         node.setUserObject(file);
 
+        // Sort by file type (directory or markdown), then sort by file name
+        Comparator<File> fileComparator = (n1, n2) -> {
+            boolean isDirectory1 = n1.isDirectory();
+            boolean isDirectory2 = n2.isDirectory();
+
+            if (isDirectory1 && !isDirectory2) {
+                return -1;
+            } else if (!isDirectory1 && isDirectory2) {
+                return 1;
+            } else {
+                return n1.getName().compareTo(n2.getName());
+            }
+        };
+
         if (file.isDirectory()) {
             File[] children = file.listFiles();
 
             if (children != null) {
+                Arrays.sort(children, fileComparator);
                 for (File child : children) {
                     if (child.isDirectory() || child.getName().endsWith(".md")) {
                         node.add(createNode(child));
